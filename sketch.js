a1bd9b426fe95298527501fd32d8f534e00ea93e
BryanBergamim <00001114405516sp@al.educacao.sp.gov.br>
2024-08-29 19:21:10 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(1000, 800);
+background("#000000");
+}
+
+function draw() {
+
+  fill("rgb(4,0,255)")
+  stroke("rgb(255,0,0)(255,255,255")
+  if(mouseIsPressed)
+ rect(mouseX,mouseY,50,50);
+
+}
